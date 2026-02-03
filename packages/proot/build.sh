ec3de91e8b2805b5f7dd3caa9ce4142f42a79049
Caten <catenhu@gmail.com>
2026-02-04 05:51:15 +0800

    Update build.sh

--- packages/proot/build.sh
+++ packages/proot/build.sh
@@ -4,11 +4,11 @@ TERMUX_PKG_DESCRIPTION="Emulate chroot, bind mount and binfmt_misc for non-root 
 TERMUX_PKG_LICENSE="GPL-2.0"
 TERMUX_PKG_MAINTAINER="Michal Bednarski @michalbednarski"
 # Just bump commit and version when needed:
-_COMMIT=228a5f28b078f4e2504de46758ce17948f73f507
+_COMMIT=b1bfe5fdfbf7a6fcda2e4f6e280f6821959041b0
 TERMUX_PKG_VERSION=5.1.107
 TERMUX_PKG_REVISION=68
-TERMUX_PKG_SRCURL=https://github.com/termux/proot/archive/${_COMMIT}.zip
-TERMUX_PKG_SHA256=39ed5eeb6ebde37be8e53591c670bf35d05869f196eff697c8dd1c20af3c0034
+TERMUX_PKG_SRCURL=https://github.com/tiny-computer/proot-termux/archive/${_COMMIT}.zip
+TERMUX_PKG_SHA256=04969b9689de3ba1adab2a3892a7cf25f9aed16876dd753e44abdaf72fafe6f9
 TERMUX_PKG_AUTO_UPDATE=false
 TERMUX_PKG_DEPENDS="libtalloc"
 TERMUX_PKG_SUGGESTS="proot-distro"
